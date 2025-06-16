4f8788b4029ce69a46b34db0a40169be118a11d6
junhyeok <junhyoklee616@gmail.com>
2025-06-16 22:17:22 +0900

    create project

---
+++ src/main/java/com/example/cookfluencerbackend/CookfluencerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.cookfluencerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookfluencerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CookfluencerBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cookfluencerbackend/CookfluencerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cookfluencerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookfluencerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
